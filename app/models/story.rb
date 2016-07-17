f50b45ab975ed7668f77d4dfd10a9fe4fcf7f1f8
Shaked Edri <ShakedEdri@Shakeds-MacBook-Pro.local>
2016-07-17 12:20:10 -0400

    fixing bucket again

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,7 +5,7 @@ class Story < ActiveRecord::Base
 	has_attached_file :image, 
 		styles: { large: "600x600>", medium: "500x500>", thumb: "100x100>" }, 
 		default_url: "/images/:style/missing.png",
-		bucket: ENV['MY_BUCKET_NAME']
+		bucket: ENV['AWS_BUCKET_ID']
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
 
